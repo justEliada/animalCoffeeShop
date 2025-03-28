c2d352bb05e235a6cb30ef5653598b591a2871cd
justEliada <eliadaballazhi@gmail.com>
2025-03-28 11:22:10 +0100

    adding new entity

--- src/main/java/com/example/ecommerce/jpa/entities/Products.java
+++ src/main/java/com/example/ecommerce/jpa/entities/Products.java
@@ -1,4 +1,24 @@
 package com.example.ecommerce.jpa.entities;
 
+import jakarta.persistence.*;
+import jdk.jfr.Category;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+@Entity
+@Table(name = "products")
 public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    private Category category; // ENUM: COFFEE, FOOD, ANIMAL_FOOD, MERCH
+
+    private int stock;
+    private String imageUrl;
 }
