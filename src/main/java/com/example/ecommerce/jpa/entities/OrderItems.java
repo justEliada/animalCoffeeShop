2daed95babd6224525df87d53cca5d72827f5592
justEliada <eliadaballazhi@gmail.com>
2025-03-30 23:38:29 +0200

    orderItems table

--- src/main/java/com/example/ecommerce/jpa/entities/OrderItems.java
+++ src/main/java/com/example/ecommerce/jpa/entities/OrderItems.java
@@ -1,4 +1,25 @@
 package com.example.ecommerce.jpa.entities;
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.Order;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "order_items")
 public class OrderItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Products product;
+
+    private int quantity;
+    private BigDecimal price;
 }
