5659e8294940ddb1e74a3870a038d308055ab9e1
justEliada <eliadaballazhi@gmail.com>
2025-03-31 23:44:39 +0200

    making

--- src/main/java/com/example/ecommerce/jpa/repositories/UserRepository.java
+++ src/main/java/com/example/ecommerce/jpa/repositories/UserRepository.java
@@ -1,4 +1,14 @@
 package com.example.ecommerce.jpa.repositories;
 
-public interface UserRepository {
+import com.example.ecommerce.jpa.entities.Customers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<Customers, UUID> {
+    Optional<Customers> findByEmail(String email);
 }
+
