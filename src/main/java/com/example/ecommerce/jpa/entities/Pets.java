9610d367d31794038ee11658580280ed590d1a88
justEliada <eliadaballazhi@gmail.com>
2025-04-02 23:17:41 +0200

    making pets class

--- src/main/java/com/example/ecommerce/jpa/entities/Pets.java
+++ src/main/java/com/example/ecommerce/jpa/entities/Pets.java
@@ -1,4 +1,21 @@
 package com.example.ecommerce.jpa.entities;
+import jakarta.persistence.*;
 
+import java.util.Date;
+import java.util.UUID;
+@Entity
+@Table(name = "pets")
 public class Pets {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private PetType species; // ENUM: CAT, DOG, RABBIT, OTHER
+
+    private int age;
+    private String description;
+    private String imageUrl;
 }
